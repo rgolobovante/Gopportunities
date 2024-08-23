5f9786a5c946869cbb7d7d045760757264058c27
rgolobovante <rgolobovante@gmail.com>
2024-08-23 11:19:44 -0300

    feat: update handler structure to separate package

--- router/routes.go
+++ router/routes.go
@@ -1,40 +1,20 @@
 package router
 
 import (
-	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rgolobovante/Gopportunities/handler"
 )
 
 func initializeRouters(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
 		//Show Opening
-		v1.GET("/opening", func (ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET opening",
-			})
-		})  
-		v1.POST("/opening", func (ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "POST opening",
-			})
-		})  
-		v1.DELETE("/opening", func (ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "DELETE opening",
-			})
-		})  
-		v1.PUT("/opening", func (ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "PUT opening",
-			})
-		})  
-		v1.GET("/openings", func (ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET openings",
-			})
-		})  		
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler )
+		v1.DELETE("/opening",handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
-	
+
 }
